b41978ddebe7f8dd10fe6a54e3c0e7199ee76415
Narendra019 <64159350+Narendra019@users.noreply.github.com>
2020-04-28 14:33:02 +0530

    Add files via upload

--- joystick.py
+++ joystick.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 26 14:37:34 2020
+Created on Tue Apr 28 14:23:22 2020
 
-@author: Narendra Chowdary
+@author: user
 """
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,31 +12,38 @@ import matplotlib.animation as animation
 import serial as sr
 import string ,sys
 import mouse, time
-
+import pyautogui as py
 sr =sr.Serial()
-sr.port = 'COM3' #Arduino serial port
+sr.port = 'COM4' #Arduino serial port
 sr.baudrate=9600;
 sr.timeout = 10 #specify timeout when using readline()
 sr.open()
+# py.moveTo(0, 756,duration=0.3886999)
 while(True):
-
- try:
-   if sr.is_open==True:
-       a=sr.readline()
-       a = ((a.decode()).strip()).split(',')
-       data=np.array([])
-       x = int(a[0])+63
-       y = int(a[1])-100 
-       s = int(a[2])
-       print(x,y)
-       cur_pos = mouse.get_position()
-       new_x=cur_pos[0]-521
-       new_y= cur_pos[1]-526
-       x_new =x
-       y_new =y
-       print(x_new,y_new)
-       mouse.move(x_new, y_new)
-  
- except KeyboardInterrupt:
-         sr.close()
-         sys.exit()
+    
+      try:
+        if sr.is_open==True:
+            a=sr.readline()
+            a = ((a.decode()).strip()).split(',')
+            data=np.array([])
+            x = int(a[0])-521
+            y = int(a[1])-526
+            s = int(a[2])
+            # if x==521 & y==526:
+            #     mouse.move(0,0,absolute=True,duration=0.3887)
+            #     exit
+            print(x,y)
+            cur_pos = mouse.get_position()
+            new_x=cur_pos[0]
+            new_y= cur_pos[1]
+            x_new=x
+            y_new=y
+            print(x_new,y_new)
+            py.moveRel(x_new,y_new,duration=0.25)
+            if s==0:
+                py.click(x_new,y_new,button='Right')
+            # events = mouse.record()
+            # print(events)
+      except KeyboardInterrupt:
+              sr.close()
+              sys.exit()
